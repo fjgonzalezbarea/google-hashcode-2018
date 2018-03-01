5e230e7d4a114987381286f20fd742ab0385b796
Juanlu Hidalgo <juan.hidalgo@piksel.com>
2018-03-01 12:57:31 +0100

    App ready to be launched using Flink 1.4.0 ;)

--- src/main/java/com/datawalk/hashcode/App.java
+++ src/main/java/com/datawalk/hashcode/App.java
@@ -1,28 +1,32 @@
 package com.datawalk.hashcode;
 
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.utils.ParameterTool;
+
 import com.datawalk.hashcode.algorithm.Algorithm;
-import com.datawalk.hashcode.model.Input;
-import com.datawalk.hashcode.model.Output;
-import com.datawalk.hashcode.utils.CLI;
 
 public class App {
 
-    private static final String INPUTS_LOCATION = "";
-    private static final Algorithm algorithm = new Algorithm();
+	private static final String INPUTS_LOCATION = "";
+	private static final Algorithm algorithm = new Algorithm();
 
-    public static void main(String[] args) {
-        System.out.println("********* GOOGLE HASHCODE 2018 *********");
+	public static void main(String[] args) throws Exception {
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		DataSet<String> input = env.readTextFile(
+				ParameterTool.fromArgs(args).get("input", "src/main/resources/myInput.txt"));
+		input.print();
+	}
 
-        CLI cli = new CLI(args);
-        try {
-            Input input = cli.parse();
-            Output output = algorithm.runIteration(input);
-            output.printSolutionToFile();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("========= Execution finished =========");
-    }
+	/*
+	 * public static void main(String[] args) {
+	 * System.out.println("********* GOOGLE HASHCODE 2018 *********");
+	 * 
+	 * CLI cli = new CLI(args); try { Input input = cli.parse(); Output output =
+	 * algorithm.runIteration(input); output.printSolutionToFile(); } catch
+	 * (Exception e) { e.printStackTrace(); }
+	 * 
+	 * System.out.println("========= Execution finished ========="); }
+	 */
 
 }
