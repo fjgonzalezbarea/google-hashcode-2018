251ea1a797443f99f747aaec676a82f84af718b2
Francisco Gonzalez <francisco.gonzalez@piksel.com>
2018-03-04 23:10:04 +0100

    Update App and Car to generate partial solution

--- src/main/java/com/datawalk/hashcode/App.java
+++ src/main/java/com/datawalk/hashcode/App.java
@@ -1,101 +1,120 @@
 package com.datawalk.hashcode;
 
+import static com.datawalk.hashcode.io.Cli.generateOutput;
+import static com.datawalk.hashcode.io.Cli.readFile;
+
 import com.datawalk.hashcode.algorithm.Algorithm;
+import com.datawalk.hashcode.exceptions.IllegalInputException;
 import com.datawalk.hashcode.model.Car;
 import com.datawalk.hashcode.model.Problem;
 import com.datawalk.hashcode.model.Ride;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 import org.apache.commons.lang3.tuple.Triple;
-import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 
 public class App {
 
-	private static final String INPUTS_LOCATION = "";
-	private static final Algorithm algorithm = new Algorithm();
-	private static Problem problem;
+    private static final String INPUTS_LOCATION = "src/main/resources/";
+    private static final String OUTPUTS_LOCATION = "src/main/resources/output-extended/";
+    private static final String INPUT_A_NAME = "a_example";
+    private static final String INPUT_B_NAME = "b_should_be_easy";
+    private static final String INPUT_C_NAME = "c_no_hurry";
+    private static final String INPUT_D_NAME = "d_metropolis";
+    private static final String INPUT_E_NAME = "e_high_bonus";
 
-	public static void main(String[] args) throws Exception {
-		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-		env.setParallelism(ParameterTool.fromArgs(args).getInt("parallelism", 1));
-		Triple<Problem, Collection<Car>, Collection<Ride>> triple = readFile(env, args);
-		DataSet<Car> cars = env.fromCollection(triple.getMiddle());
-		DataSet<Ride> rides = env.fromCollection(triple.getRight());
-		problem = triple.getLeft();
-		System.out.println(problem);
-		cars.print();
-		rides.print();
-	}
+    private static final String INPUT = ".in";
+    private static final String OUTPUT = ".out";
 
-	/*
-	 * public static void main(String[] args) {
-	 * System.out.println("********* GOOGLE HASHCODE 2018 *********");
-	 * 
-	 * CLI cli = new CLI(args); try { Input input = cli.parse(); Output output =
-	 * algorithm.runIteration(input); output.printSolutionToFile(); } catch
-	 * (Exception e) { e.printStackTrace(); }
-	 * 
-	 * System.out.println("========= Execution finished ========="); }
-	 */
+    private static final Algorithm algorithm = new Algorithm();
+    private static Problem problem;
 
-	static Triple<Problem, Collection<Car>, Collection<Ride>> readFile(ExecutionEnvironment env, String[] args) {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(ParameterTool.fromArgs(args).getInt("parallelism", 1));
+        String filePath = ParameterTool.fromArgs(args)
+            .get("input", "src/main/resources/a_example.in");
+        runProgram(INPUTS_LOCATION + INPUT_A_NAME + INPUT, OUTPUTS_LOCATION + INPUT_A_NAME + OUTPUT);
+        runProgram(INPUTS_LOCATION + INPUT_B_NAME + INPUT, OUTPUTS_LOCATION + INPUT_B_NAME + OUTPUT);
+        runProgram(INPUTS_LOCATION + INPUT_C_NAME + INPUT, OUTPUTS_LOCATION + INPUT_C_NAME + OUTPUT);
+        runProgram(INPUTS_LOCATION + INPUT_D_NAME + INPUT, OUTPUTS_LOCATION + INPUT_D_NAME + OUTPUT);
+        runProgram(INPUTS_LOCATION + INPUT_E_NAME + INPUT, OUTPUTS_LOCATION + INPUT_E_NAME + OUTPUT);
+    }
 
-		Collection<Car> cars = Lists.newArrayList();
-		Collection<Ride> rides = Lists.newArrayList();
-		Problem problem = null;
-		int numLine = -1;
-		try (BufferedReader br = new BufferedReader(
-				new FileReader(ParameterTool.fromArgs(args).get("input", "src/main/resources/a_example.in")))) {
+    private static void runProgram(String inputFile, String outputFilñe)
+        throws IllegalInputException, FileNotFoundException {
+        Triple<Problem, Collection<Car>, Collection<Ride>> triple = readFile(inputFile);
+        problem = triple.getLeft();
+        Collection<Car> cars = triple.getMiddle();
+        Collection<Ride> rides = triple.getRight();
+        createSolution(cars, rides, problem.getSteps());
+        /*for (int step = 0; step < problem.getSteps(); step++) {
+			processCars(triple, step);
+		}*/
+        //System.out.println(cars);
+        generateOutput(cars, outputFilñe);
+        System.out.println("Total score --> " + score(cars, problem.getBonus()));
+    }
 
-			String sCurrentLine;
-			boolean firstLine = true;
-			while ((sCurrentLine = br.readLine()) != null) {
-				if (firstLine) {
-					problem = processFirstLine(cars, sCurrentLine);
-					firstLine = false;
-				} else {
-					processRideLines(rides, sCurrentLine, numLine);
-				}
-				numLine++;
-			}
+    private static void createSolution(Collection<Car> cars, Collection<Ride> rides, Integer numnberOfSteps) {
+        IntStream.range(0, numnberOfSteps).forEach(step -> createSolutionForStep(cars, rides, step));
+    }
 
-			return Triple.of(problem, cars, rides);
+    private static void createSolutionForStep(Collection<Car> cars, Collection<Ride> rides, Integer step) {
+        cars.forEach(car -> createSolutionForCar(car, rides, step));
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+    private static void createSolutionForCar(Car car, Collection<Ride> rides, Integer step) {
+        if (car.isAvailable(step)) {
+            AtomicInteger atomicStep = new AtomicInteger(step);
+            Collections.sort((List<Ride>) rides, createRideComparator(car, atomicStep.get()));
+            if (!rides.isEmpty()) {
+                setNextRideToCar(car, rides, step);
+            }
+        }
+    }
 
-	}
+    private static void setNextRideToCar(Car car, Collection<Ride> rides, Integer step) {
+        Ride ride = rides.iterator().next();
+        ride.setTimeTaken(step);
+        car.takeRide(step, ride);
+        rides.remove(ride);
+    }
 
-	private static void processRideLines(Collection<Ride> rides, String sCurrentLine, int numLine) {
-		String[] line = sCurrentLine.split(" ");
-		Ride ride = new Ride();
-		ride.setStartPositionX(Integer.parseInt(line[0]));
-		ride.setStartPositionY(Integer.parseInt(line[1]));
-		ride.setFinishPositionX(Integer.parseInt(line[2]));
-		ride.setFinishPositionY(Integer.parseInt(line[3]));
-		ride.setStartTime(Integer.parseInt(line[4]));
-		ride.setFinishTime(Integer.parseInt(line[5]));
-		ride.setId(new Long(numLine));
-		rides.add(ride);
-	}
+    private static void processCar(Triple<Problem, Collection<Car>, Collection<Ride>> triple,
+        int step, Car car) {
+        if (car.isAvailable(step)) {
+            AtomicInteger step1 = new AtomicInteger(step);
+            Collections.sort((List<Ride>) triple.getRight(), (r1, r2) -> {
+                Long p1 = car.peso(r1, step1.get());
+                Long p2 = car.peso(r2, step1.get());
+                return p1.compareTo(p2);
+            });
+            if (!triple.getRight().isEmpty()) {
+                Ride ride = triple.getRight().iterator().next();
+                ride.setTimeTaken(step);
+                car.takeRide(step, ride);
+                triple.getRight().remove(ride);
+            }
+        }
+    }
 
-	private static Problem processFirstLine(Collection<Car> cars, String sCurrentLine) {
-		Problem problem;
-		String[] line = sCurrentLine.split(" ");
-		for (int i = 0; i < Integer.parseInt(line[2]); i++) {
-			cars.add(new Car(new Long(i)));
-		}
-		problem = new Problem(Integer.parseInt(line[5]), Integer.parseInt(line[4]), Integer.parseInt(line[0]),
-				Integer.parseInt(line[1]));
+    public static int score(Collection<Car> cars, int bonus) {
+        return cars.stream().mapToInt(car -> car.score(bonus)).sum();
+    }
 
-		return problem;
-	}
+    private static Comparator<Ride> createRideComparator(Car car, Integer step) {
+        return (r1, r2) -> {
+            Long p1 = car.peso(r1, step);
+            Long p2 = car.peso(r2, step);
+            return p1.compareTo(p2);
+        };
+    }
 
 }
--- src/main/java/com/datawalk/hashcode/model/Car.java
+++ src/main/java/com/datawalk/hashcode/model/Car.java
@@ -2,22 +2,79 @@ package com.datawalk.hashcode.model;
 
 import java.util.LinkedList;
 import java.util.List;
-import lombok.Value;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
-@Value
+@Data
+@Slf4j
 public class Car {
-    Long id;
-    List<Ride> ridesTaken;
+	Long id;
+	List<Ride> ridesTaken;
+	Integer stepToBeFree;
 
-    public Car(Long id) {
-        this.id = id;
-        ridesTaken = new LinkedList<>();
+	public Car(Long id) {
+		this.id = id;
+		this.ridesTaken = new LinkedList<>();
+		this.stepToBeFree = 0;
+	}
+
+	public void takeRide(int currentStep, Ride ride) {
+	    if(ridesTaken.isEmpty()) {
+	        stepToBeFree = currentStep + Math.abs(ride.startPositionX - ride.startPositionY) + ride.distance() + 1;
+        } else {
+            Ride lastRide = ridesTaken.get(ridesTaken.size() - 1);
+            stepToBeFree = currentStep + ride
+                .dinstanceFromOrigin(lastRide.finishPositionX, lastRide.finishPositionY) + ride
+                .distance() + 1;
+        }
+        ridesTaken.add(ride);
+	}
+
+	public boolean isAvailable(int currentStep) {
+	    return stepToBeFree <= currentStep;
     }
 
-    public int score(int bonusValue) {
-        return ridesTaken.stream()
-            .map(ride -> ride.distance() + bonusValue)
-            .reduce((a, b) -> a+b)
-            .orElse(0);
+	public Long peso(Ride ride, int currentStep) {
+		int lastRideX = getLastRide().map(lastRide -> lastRide.getStartPositionX()).orElse(0);
+		int lastRideY = getLastRide().map(lastRide -> lastRide.getStartPositionY()).orElse(0);
+		int distanceLstRideToRide = ride.dinstanceFromOrigin(lastRideX, lastRideY);
+
+		return Long.valueOf(ride.getFinishTime() - (ride.distance() + distanceLstRideToRide + 1 + currentStep));
+	}
+
+    public int distanceToRide(int currentStep, Ride ride) {
+        if(ridesTaken.isEmpty()) {
+            return currentStep + Math.abs(ride.startPositionX - ride.startPositionY) + ride.distance() + 1;
+        }
+	    Ride lastRide = ridesTaken.get(ridesTaken.size()-1);
+        return currentStep + ride.dinstanceFromOrigin(lastRide.finishPositionX, lastRide.finishPositionY) + ride.distance() + 1;
     }
+
+    public Optional<Ride> getLastRide() {
+	    if(ridesTaken.size() == 0) {
+	        return Optional.empty();
+        }
+        return Optional.of(ridesTaken.get(ridesTaken.size()-1));
+    }
+
+	public int score(int bonus) {
+		return ridesTaken.stream().map(ride -> ride.distance() + bonus(ride, bonus)).reduce((a, b) -> a + b).orElse(0);
+	}
+
+	public String toString() {
+	    return "Car " + id + " took rides --> " + ridesTaken.stream().map(ride -> ride.id.toString()).collect(Collectors.joining(",")) + "\n";
+    }
+
+	private int bonus(Ride ride, int bonus) {
+		if (ride.startTime == ride.timeTaken) {
+			//return ride.finishTime - (ride.startTime + ride.distance() + 1);
+			return bonus;
+		} else if (ride.startTime > ride.timeTaken) {
+			//System.out.println("Ride taken at " + ride.timeTaken + " and should be taken at " + ride.startTime + " for ride " + ride.getId());
+			//throw new IllegalStateException("Car should wait for start time of the ride");
+		}
+		return 0;
+	}
 }
