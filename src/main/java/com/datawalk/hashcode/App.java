d99758021e8ede0188b14d4b499c4e505c8a0bb8
Francisco Gonzalez <francisco.gonzalez@piksel.com>
2018-03-07 00:41:02 +0100

    Created new approach selecting first round the closest ride to each car. Second round ahead, for each ride select a car which could arrive on startTime. Otherwise select the closest one. Much slower solution. Actually no results yet

--- src/main/java/com/datawalk/hashcode/App.java
+++ src/main/java/com/datawalk/hashcode/App.java
@@ -13,11 +13,14 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.commons.lang3.tuple.Triple;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 
 public class App {
 
@@ -42,9 +45,9 @@ public class App {
             .get("input", "src/main/resources/a_example.in");
         runProgram(INPUTS_LOCATION + INPUT_A_NAME + INPUT, OUTPUTS_LOCATION + INPUT_A_NAME + OUTPUT);
         runProgram(INPUTS_LOCATION + INPUT_B_NAME + INPUT, OUTPUTS_LOCATION + INPUT_B_NAME + OUTPUT);
-        runProgram(INPUTS_LOCATION + INPUT_C_NAME + INPUT, OUTPUTS_LOCATION + INPUT_C_NAME + OUTPUT);
+        //runProgram(INPUTS_LOCATION + INPUT_C_NAME + INPUT, OUTPUTS_LOCATION + INPUT_C_NAME + OUTPUT);
         runProgram(INPUTS_LOCATION + INPUT_D_NAME + INPUT, OUTPUTS_LOCATION + INPUT_D_NAME + OUTPUT);
-        runProgram(INPUTS_LOCATION + INPUT_E_NAME + INPUT, OUTPUTS_LOCATION + INPUT_E_NAME + OUTPUT);
+        //runProgram(INPUTS_LOCATION + INPUT_E_NAME + INPUT, OUTPUTS_LOCATION + INPUT_E_NAME + OUTPUT);
     }
 
     private static void runProgram(String inputFile, String outputFilñe)
@@ -54,20 +57,26 @@ public class App {
         Collection<Car> cars = triple.getMiddle();
         Collection<Ride> rides = triple.getRight();
         createSolution(cars, rides, problem.getSteps());
-        /*for (int step = 0; step < problem.getSteps(); step++) {
-			processCars(triple, step);
-		}*/
-        //System.out.println(cars);
         generateOutput(cars, outputFilñe);
         System.out.println("Total score --> " + score(cars, problem.getBonus()));
     }
 
     private static void createSolution(Collection<Car> cars, Collection<Ride> rides, Integer numnberOfSteps) {
-        IntStream.range(0, numnberOfSteps).forEach(step -> createSolutionForStep(cars, rides, step));
+        createSolutionForStep(cars, rides, 0);
+        IntStream.range(1, numnberOfSteps).forEach(step -> {
+            System.out.println("Step --> " + step);
+            createSolutionForStep(cars, rides, step);
+        });
     }
 
     private static void createSolutionForStep(Collection<Car> cars, Collection<Ride> rides, Integer step) {
-        cars.forEach(car -> createSolutionForCar(car, rides, step));
+        //cars.forEach(car -> createSolutionForCar(car, rides, step));
+        Collection<Ride> copyOfRides = Lists.newArrayList(rides);
+        copyOfRides.forEach(ride -> {
+            if(createSolutionForRide(cars, ride, step)) {
+                rides.remove(ride);
+            }
+        });
     }
 
     private static void createSolutionForCar(Car car, Collection<Ride> rides, Integer step) {
@@ -80,10 +89,38 @@ public class App {
         }
     }
 
-    private static void setNextRideToCar(Car car, Collection<Ride> rides, Integer step) {
-        Ride ride = rides.iterator().next();
+    private static boolean createSolutionForRide(Collection<Car> cars, Ride ride, Integer step) {
+        Optional<Car> preferredCar = cars.stream()
+            .filter(car -> car.isAvailable(step))
+            .filter(car -> ride.dinstanceFromOrigin(car.lastRideCoordinates().getLeft(),
+                car.lastRideCoordinates().getRight()) + step == ride.getStartTime())
+            .findAny();
+
+        if(preferredCar.isPresent()) {
+            assignRideToCar(ride, step, preferredCar.get());
+            return true;
+        } else {
+            AtomicInteger atomicStep = new AtomicInteger(step);
+            List<Car> availableCars = cars.stream().filter(car -> car.isAvailable(step)).collect(
+                Collectors.toList());
+            if (!availableCars.isEmpty()) {
+                Collections.sort(availableCars, createCarComparator(ride, atomicStep.get()));
+                Car closestCar = availableCars.get(0);
+                assignRideToCar(ride, step, closestCar);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void assignRideToCar(Ride ride, Integer step, Car car) {
         ride.setTimeTaken(step);
         car.takeRide(step, ride);
+    }
+
+    private static void setNextRideToCar(Car car, Collection<Ride> rides, Integer step) {
+        Ride ride = rides.iterator().next();
+        assignRideToCar(ride, step, car);
         rides.remove(ride);
     }
 
@@ -113,6 +150,14 @@ public class App {
         return (r1, r2) -> {
             Long p1 = car.peso(r1, step);
             Long p2 = car.peso(r2, step);
+            return p1.compareTo(p2);
+        };
+    }
+
+    private static Comparator<Car> createCarComparator(Ride ride, Integer step) {
+        return (c1, c2) -> {
+            Long p1 = c1.peso(ride, step);
+            Long p2 = c2.peso(ride, step);
             return p1.compareTo(p2);
         };
     }
--- src/main/java/com/datawalk/hashcode/model/Car.java
+++ src/main/java/com/datawalk/hashcode/model/Car.java
@@ -6,23 +6,27 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Data
 @Slf4j
 public class Car {
-	Long id;
-	List<Ride> ridesTaken;
-	Integer stepToBeFree;
 
-	public Car(Long id) {
-		this.id = id;
-		this.ridesTaken = new LinkedList<>();
-		this.stepToBeFree = 0;
-	}
+    Long id;
+    List<Ride> ridesTaken;
+    Integer stepToBeFree;
 
-	public void takeRide(int currentStep, Ride ride) {
-	    if(ridesTaken.isEmpty()) {
-	        stepToBeFree = currentStep + Math.abs(ride.startPositionX - ride.startPositionY) + ride.distance() + 1;
+    public Car(Long id) {
+        this.id = id;
+        this.ridesTaken = new LinkedList<>();
+        this.stepToBeFree = 0;
+    }
+
+    public void takeRide(int currentStep, Ride ride) {
+        if (ridesTaken.isEmpty()) {
+            stepToBeFree =
+                currentStep + Math.abs(ride.startPositionX - ride.startPositionY) + ride.distance()
+                    + 1;
         } else {
             Ride lastRide = ridesTaken.get(ridesTaken.size() - 1);
             stepToBeFree = currentStep + ride
@@ -30,47 +34,47 @@ public class Car {
                 .distance() + 1;
         }
         ridesTaken.add(ride);
-	}
-
-	public boolean isAvailable(int currentStep) {
-	    return stepToBeFree <= currentStep;
     }
 
-	public Long peso(Ride ride, int currentStep) {
-		int lastRideX = getLastRide().map(lastRide -> lastRide.getFinishPositionX()).orElse(0);
-		int lastRideY = getLastRide().map(lastRide -> lastRide.getFinishPositionY()).orElse(0);
-		int distanceLstRideToRide = ride.dinstanceFromOrigin(lastRideX, lastRideY);
+    public boolean isAvailable(int currentStep) {
+        return stepToBeFree <= currentStep;
+    }
 
-		return Long.valueOf(ride.getFinishTime() - (ride.distance() + distanceLstRideToRide + 1 + currentStep));
-	}
+    public Long peso(Ride ride, int currentStep) {
+        int lastRideX = getLastRide().map(lastRide -> lastRide.getFinishPositionX()).orElse(0);
+        int lastRideY = getLastRide().map(lastRide -> lastRide.getFinishPositionY()).orElse(0);
+        int distanceLstRideToRide = ride.dinstanceFromOrigin(lastRideX, lastRideY);
 
-    public int distanceToRide(int currentStep, Ride ride) {
-        if(ridesTaken.isEmpty()) {
-            return currentStep + Math.abs(ride.startPositionX - ride.startPositionY) + ride.distance() + 1;
-        }
-	    Ride lastRide = ridesTaken.get(ridesTaken.size()-1);
-        return currentStep + ride.dinstanceFromOrigin(lastRide.finishPositionX, lastRide.finishPositionY) + ride.distance() + 1;
+        return Long.valueOf(
+            ride.getFinishTime() - (ride.distance() + distanceLstRideToRide + 1 + currentStep));
     }
 
     public Optional<Ride> getLastRide() {
-	    if(ridesTaken.size() == 0) {
-	        return Optional.empty();
+        if (ridesTaken.size() == 0) {
+            return Optional.empty();
         }
-        return Optional.of(ridesTaken.get(ridesTaken.size()-1));
+        return Optional.of(ridesTaken.get(ridesTaken.size() - 1));
     }
 
-	public int score(int bonus) {
-		return ridesTaken.stream().map(ride -> ride.distance() + bonus(ride, bonus)).reduce((a, b) -> a + b).orElse(0);
-	}
-
-	public String toString() {
-	    return "Car " + id + " took rides --> " + ridesTaken.stream().map(ride -> ride.id.toString()).collect(Collectors.joining(",")) + "\n";
+    public Pair<Integer, Integer> lastRideCoordinates() {
+        return getLastRide().map(ride -> Pair.of(ride.finishPositionX, ride.finishPositionY))
+            .orElse(Pair.of(0, 0));
     }
 
-	private int bonus(Ride ride, int bonus) {
-		if (ride.startTime == ride.timeTaken) {
-			return bonus;
-		}
-		return 0;
-	}
+    public int score(int bonus) {
+        return ridesTaken.stream().map(ride -> ride.distance() + bonus(ride, bonus))
+            .reduce((a, b) -> a + b).orElse(0);
+    }
+
+    public String toString() {
+        return "Car " + id + " took rides --> " + ridesTaken.stream()
+            .map(ride -> ride.id.toString()).collect(Collectors.joining(",")) + "\n";
+    }
+
+    private int bonus(Ride ride, int bonus) {
+        if (ride.startTime == ride.timeTaken) {
+            return bonus;
+        }
+        return 0;
+    }
 }
